97e88fe5bcd7c848f14562a22338719cf0cc5287
gabriel-bizioo <gabriel.bizio@ufpr.br>
2025-05-24 23:03:29 -0300

    tomando forma

--- main.c
+++ main.c
@@ -37,13 +37,14 @@ int min(int *valores, int n) {
     return min;
 }
 
-int B_dada(int c_atual, int *valores, int n, int n_restantes) {
+int B_dada(estado_t *estado_t) {
 
-    return c_atual + min(valores, n) * n_restantes;
+    return 0;
 }
 
-estado_t *copiaestado(estado_t *estado) {
+estado_t *copia_estado(estado_t *estado) {
     estado_t *copia = malloc(sizeof(estado_t));
+
     int nPapeis = estado->nPapeis;
     int nGrupos = estado->nGrupos;
     int nAtores = estado->nAtores;
@@ -63,16 +64,34 @@ estado_t *copiaestado(estado_t *estado) {
     return copia;
 }
 
-void branch_and_bound(estado_t *estado, ator *atores, int nAtores,
-        int *func(int, int*, int)) {
+void branch_and_bound(estado_t *estado, ator *atores, int nAtores, int *func(int, int*, int)) {
     void *bound = func;
     int menor_custo = INFINITY;
-    
-    if((!estado->nPapeis) && (!estado->nAtores) && (!estado->nGrupos)) {
-        estado_t *novo_estado = copiaestado(estado);
 
-        for(int i = 0; i < nAtores; i++) {
-            
+    if((!estado->nPapeis) && (!estado->nAtores) && (!estado->nGrupos)) {
+
+        //O(infinito e alem) se der tempo implementar com tabela hash
+        for(int i=nAtores; i>0; i--) {
+            ator ator = atores[i];
+            int nPapeis = ator.nPapeis;
+
+            for(int j=0; j < nPapeis; j++) {
+                if(!estado->papeis[ator.iPapeis[j]]) {
+                    estado_t *novo_estado = copia_estado(estado);
+                    //ator *novo_atores = copia_atores(atores)
+
+                    int nGrupos = ator.nGrupos;
+                    for(int z=0; z<nGrupos; z++) {
+                        if(!novo_estado->grupos[ator.iGrupos[z]]) // Aaa se desse pra fazer em python...
+                            novo_estado->grupos[ator.iGrupos[z]] = 1;
+                    }
+                    //adiciona(novo_estado->atores, ator);
+                    //remove(atores, ator);
+
+                    int novo_nAtores = nAtores - 1;
+                    branch_and_bound(novo_estado, atores, novo_nAtores--, func);
+                }
+            }
         }
     }
 
